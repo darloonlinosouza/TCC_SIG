19670b12c12b7595dd82b010e7e2d0fdbd4c6cba
Darloon Lino <darloon.lino.souza@hotmail.com>
2023-03-22 16:49:23 -0300

    tela do mapa - teste

--- app/src/main/java/com/darloonlino/tcc_sig/MainActivity.java
+++ app/src/main/java/com/darloonlino/tcc_sig/MainActivity.java
@@ -3,10 +3,14 @@ package com.darloonlino.tcc_sig;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button botaoAcessar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,5 +19,14 @@ public class MainActivity extends AppCompatActivity {
         // esconde o menu superior
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
+
+        botaoAcessar = findViewById(R.id.botaoAcessar);
+        botaoAcessar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in = new Intent(MainActivity.this, MapActivity.class);
+                startActivity(in);
+            }
+        });
     }
 }
